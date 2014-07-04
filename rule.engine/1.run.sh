7fec4322a2bbad5cf7fc4c3c758bb931f118f484
slawek <janusz.slawek@gmail.com>
2014-07-04 12:45:32 -0700

    change run.sh script

--- rule.engine/1.run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./storm-0.9.0.1/bin/storm jar myjob.jar rule.engine.RuleEngineTopology RuleEngine &>> ./log/server.log &
---
+++ rule.engine/bin/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# old, do not use
+#./storm-0.9.0.1/bin/storm jar myjob.jar rule.engine.RuleEngineTopology RuleEngine &>> ./log/server.log &
+
