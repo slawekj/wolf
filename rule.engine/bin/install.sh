be6359ccc908a9f435d32cfd4e8a1838ebcd9a95
slawek <janusz.slawek@gmail.com>
2014-07-05 15:05:07 -0700

    update deployment scripts for rule engine

--- rule.engine/bin/install.sh
+++ rule.engine/bin/install.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # remove all previous java
 sudo apt-get purge openjdk-\* icedtea-\* icedtea6-\*
 
 # install java 1.7
 sudo apt-get install openjdk-7-jdk
-
 
 sudo apt-get install zookeeper
 # zookeeper conf is in /etc/zookeeper/conf/zoo.cfg
@@ -18,23 +17,29 @@ sudo /usr/share/zookeeper/bin/zkServer.sh start
 # check if zookeeper is running
 echo stat | nc localhost 2181
 
-# do I really need ZeroMQ? I hope not
-# ignore ZeroMQ installation
-
-
+# we will need storm
 sudo apt-get install unzip
 wget http://mirror.symnds.com/software/Apache/incubator/storm/apache-storm-0.9.2-incubating/apache-storm-0.9.2-incubating.zip
 unzip apache-storm-0.9.2-incubating.zip
 
+# we will also need storm-kafka integration jars in mvn repository
+# this step might be done on dev machine, not production server
+# this step will install storm-core 0.9.3 and storm-kafka 0.9.3 and kafka_2.9.2 0.8.1.1 to local mvn repository
+sudo apt-get install maven2 
+git clone https://github.com/apache/incubator-storm.git
+cd incubator-storm && mvn clean install -DskipTests=true && cd
+# once this is done we can install rule engine
+cd wolf/rule.engine && mvn clean install
+
 # run nimbus
-./apache-storm-0.9.2-incubating/bin/storm nimbus &
-sleep 10
+#./apache-storm-0.9.2-incubating/bin/storm nimbus &
+#sleep 10
 # run storm ui
-./apache-storm-0.9.2-incubating/bin/storm ui &
-sleep 10
+#./apache-storm-0.9.2-incubating/bin/storm ui &
+#sleep 10
 # run storm supervisor
-./apache-storm-0.9.2-incubating/bin/storm supervisor &
-sleep 10
+#./apache-storm-0.9.2-incubating/bin/storm supervisor &
+#sleep 10
 # now build topology
 
 
--- rule.engine/bin/run.sh
+++ rule.engine/bin/run.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# old, do not use
-#./storm-0.9.0.1/bin/storm jar myjob.jar rule.engine.RuleEngineTopology RuleEngine &>> ./log/server.log &
+# run nimbus
+./apache-storm-0.9.2-incubating/bin/storm nimbus &
+sleep 10
+# run storm ui
+./apache-storm-0.9.2-incubating/bin/storm ui &
+sleep 10
+# run storm supervisor
+./apache-storm-0.9.2-incubating/bin/storm supervisor &
+sleep 10
+# now build topology
+./apache-storm-0.9.2-incubating/bin storm jar ./wolf/rule.engine/target/rule.engine-0.0.1-SNAPSHOT-jar-with-dependencies.jar rule.engine.RuleEngineTopology RuleEngine
 
